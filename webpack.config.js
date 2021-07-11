1cde7686a5a103feb9696754ad5f6050a0f3471e
HobinLee <viny5120@gmail.com>
2021-07-11 13:19:19 +0900

    setup: Set TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,25 @@ const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      options: {
-      },
-      exclude: ['/node_modules'],
-    }],
+    rules: [],
   },
   plugins: [],
   optimization: {},
